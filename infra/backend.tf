a3f6a111e5e88f4779f88b14ce815979038aa2bd
Fola Azeez <foladevops@gmail.com>
2026-02-12 11:23:35 +0000

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "REPLACE_ME"
+    bucket         = "pipelineguard-chatgpt-fola"
     key            = "platform-foundations/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "REPLACE_ME"
