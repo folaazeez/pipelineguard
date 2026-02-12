92bb5a8b558376a6c30fc140eb2598080b6f7407
Fola Azeez <foladevops@gmail.com>
2026-02-12 11:10:10 +0000

    Create main.tf

---
+++ infra/bootstrap/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "REPLACE_ME_UNIQUE_BUCKET_NAME"
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "REPLACE_ME_LOCK_TABLE"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "state_bucket" {
+  value = aws_s3_bucket.tf_state.bucket
+}
+
+output "lock_table" {
+  value = aws_dynamodb_table.tf_lock.name
+}
