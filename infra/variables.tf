8813f833ca989d45556e04825652631d5561b423
Fola Azeez <foladevops@gmail.com>
2026-02-15 10:24:13 +0000

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   type    = string
   default = "eu-west-2"
 }
+
+variable "policy_gate_image" {
+  type        = string
+  description = "Container image URI for policy-gate (including tag)"
+}
