a4f7a1e619c5468df4fe91e4b753944b44b4d32e
Fola Azeez <foladevops@gmail.com>
2026-02-18 14:15:30 +0000

    updated

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -86,11 +86,6 @@ resource "aws_iam_role" "github_actions_tf" {
   })
 }
 
-# resource "aws_iam_role_policy_attachment" "tf_admin" {
-#   role       = aws_iam_role.github_actions_tf.name
-#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-# }
-
 output "github_actions_role_arn" {
   value = aws_iam_role.github_actions_tf.arn
 }
